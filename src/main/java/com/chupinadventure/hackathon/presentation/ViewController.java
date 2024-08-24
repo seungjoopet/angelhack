981416d7c71efc084e9c5bdf046cab211f3ec84d
seungjoo.jeong <seungjoo.jeong@navercorp.com>
2024-08-25 00:01:53 +0900

    wip

--- src/main/java/com/chupinadventure/hackathon/presentation/ViewController.java
+++ src/main/java/com/chupinadventure/hackathon/presentation/ViewController.java
@@ -3,12 +3,18 @@ package com.chupinadventure.hackathon.presentation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class ViewController {
 
 
-    @RequestMapping(value = {"/", "/collect/**"})
-    public String get() {
+    @RequestMapping(value = {"/", "/collect/**", "/edit/**", "/complete"})
+    public String get(final HttpServletResponse response) {
+
+        response.setHeader("Cache-Control", "no-cache");
+        response.addHeader("Cache-Control", "no-store");
+
         return "index.html";
     }
 
