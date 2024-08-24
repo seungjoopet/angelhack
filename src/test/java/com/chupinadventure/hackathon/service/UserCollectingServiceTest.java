5302d83ea740970dfcfd17a7be5cf8f6fc049747
monorisk <monorisk@gmail.com>
2024-08-25 06:08:19 +0900

    update assets

---
+++ src/test/java/com/chupinadventure/hackathon/service/UserCollectingServiceTest.java
@@ -0,0 +1,17 @@
+package com.chupinadventure.hackathon.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCollectingServiceTest {
+
+    @Autowired
+    private UserCollectingService service;
+
+    @Test
+    void test() {
+        service.end(new UserLocationEndCommand("dummy", 23));
+    }
+}
