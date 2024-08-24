2ff17de32fc7581b38ec59a4cb5d7622b6738c7e
monorisk <monorisk@gmail.com>
2024-08-25 07:55:38 +0900

    fix

--- src/main/java/com/chupinadventure/hackathon/service/UserLocationEndCommand.java
+++ src/main/java/com/chupinadventure/hackathon/service/UserLocationEndCommand.java
@@ -1,5 +1,7 @@
 package com.chupinadventure.hackathon.service;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
@@ -7,7 +9,9 @@ public final class UserLocationEndCommand {
     private final String userId;
     private final long collectId;
 
-    public UserLocationEndCommand(final String userId, final long collectId) {
+    @JsonCreator
+    public UserLocationEndCommand(@JsonProperty("userId") final String userId,
+                                  @JsonProperty("collectId") final long collectId) {
         this.userId = userId;
         this.collectId = collectId;
     }
